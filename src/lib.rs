658b349db10d88d63b27b15a33bdb26c11a38b17
Maxence Frenette <maxence.frenette@gmail.com>
2018-03-30 15:57:42 -0400

    Change Engine::new() to take a handle ref

--- src/lib.rs
+++ src/lib.rs
@@ -27,9 +27,7 @@ pub struct Engine {
 }
 
 impl Engine {
-    #[async]
-    // TODO: make the handle passable by reference
-    pub fn new(path: String, handle: Handle) -> Result<Engine, ()> {
+    pub fn new(path: String, handle: &Handle) -> impl Future<Item = Engine, Error = ()> {
         let mut process = Command::new(path)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
@@ -45,10 +43,12 @@ impl Engine {
             lines,
         };
 
-        engine = await!(engine.write("uci\n".to_string())).expect("failed to write to engine");
-        engine = await!(engine.wait_for(EngineMessage::UciOk)).expect("failed to wait for uciok");
+        async_block! {
+            engine = await!(engine.write("uci\n".to_string())).expect("failed to write to engine");
+            engine = await!(engine.wait_for(EngineMessage::UciOk)).expect("failed to wait for uciok");
 
-        Ok(engine)
+            Ok(engine)
+        }
     }
 
     /// Waits for the engine to be ready to accept more commands.
@@ -154,7 +154,7 @@ impl Engine {
     /// 
     /// fn main() {
     ///     let mut core = Core::new().unwrap();
-    ///     let future = Engine::new("stockfish".to_string(), core.handle())
+    ///     let future = Engine::new("stockfish".to_string(), &core.handle())
     ///         .and_then(|engine| engine.write("go nodes 1000".to_string()));
     ///     core.run(future);
     /// }
@@ -230,7 +230,7 @@ mod tests {
     #[test]
     fn engine_test() {
         let mut core = Core::new().unwrap();
-        let engine_future = Engine::new("stockfish".to_string(), core.handle());
+        let engine_future = Engine::new("stockfish".to_string(), &core.handle());
         let mut engine = core.run(engine_future).unwrap();
         engine = core.run(engine.go("nodes 1000".to_string())).unwrap().0;
         engine = core.run(engine.ponder_hit()).unwrap();
